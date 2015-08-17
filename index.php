6e01c14e9525cdf2c60bd010cafc6526dec57e4d
Franklin <frkmrd@hotmail.com>
2015-08-17 09:22:48 -0400

    17-08-2015 version inicial

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/menu.css">
+	<script type="javascript" src="menu.js"></script>
+</head>
+<body>
+
+<div id="encabezado">
+	<div id="nombre"> <h1> CONSTRUCTORA QUINTANILLA</h1></div>
+	<div id="slider"></div>
+	<div id="menu">
+		
+<?php  
+	include("menu.html");
+
+?>
+
+	</div>
+</div>
+<div id="cuerpo"></div>
+<div id="pie"></div>
+<div id="mapa"> </div>
+</body>
+</html>
